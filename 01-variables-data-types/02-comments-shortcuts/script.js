15f7b2c82b32c5beee9cdbce61da4b0287321dbd
deepak2090 <deepak.2090@gmail.com>
2024-12-23 22:47:02 -0500

    test update

--- 01-variables-data-types/02-comments-shortcuts/script.js
+++ 01-variables-data-types/02-comments-shortcuts/script.js
@@ -1,10 +1,18 @@
 // This is a single line of code
 
-/*
-Multi-line comment
+
+//Multi-line comment
 console.log(100);
 
 console.log('Hello World');
 
 console.log(20, 'Hello', true);
-*/
+
+console.group('simple');
+console.log(100);
+console.log('Hello World');
+console.log(20, 'Hello', true);
+console.log(100);
+console.log('Hello World');
+console.log(20, 'Hello', true);
+console.groupEnd();
